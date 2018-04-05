c1472075dc89ebafc9c7c2b8fd8266c6000d0b8a
Inozitol <Pavek789@seznam.cz>
2018-04-05 19:57:39 +0200

    Update

---
+++ cmake-build-debug/CMakeFiles/CityBomber.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "O:/Projekty/CityBomber/Bomb.cpp" "O:/Projekty/CityBomber/cmake-build-debug/CMakeFiles/CityBomber.dir/Bomb.cpp.obj"
+  "O:/Projekty/CityBomber/Building.cpp" "O:/Projekty/CityBomber/cmake-build-debug/CMakeFiles/CityBomber.dir/Building.cpp.obj"
+  "O:/Projekty/CityBomber/Plane.cpp" "O:/Projekty/CityBomber/cmake-build-debug/CMakeFiles/CityBomber.dir/Plane.cpp.obj"
+  "O:/Projekty/CityBomber/main.cpp" "O:/Projekty/CityBomber/cmake-build-debug/CMakeFiles/CityBomber.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "O:/Libraries/SFML-2.4.2/install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/CityBomber.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CityBomber.dir/main.cpp.obj"
+  "CMakeFiles/CityBomber.dir/Plane.cpp.obj"
+  "CMakeFiles/CityBomber.dir/Bomb.cpp.obj"
+  "CMakeFiles/CityBomber.dir/Building.cpp.obj"
+  "CityBomber.pdb"
+  "CityBomber.exe"
+  "CityBomber.exe.manifest"
+  "libCityBomber.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CityBomber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
